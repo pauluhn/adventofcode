0908e1b9463eeb60a9faa689732e0b3f96f6237f
Paul Uhn <paulu@moj.io>
2023-12-01 09:03:29 -0600

    Initial Xcode project

---
+++ adventofcode/adventofcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  adventofcodeApp.swift
+//  adventofcode
+//
+//  Created by Paul U on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct adventofcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
