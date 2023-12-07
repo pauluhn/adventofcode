23ad8a03d14decc5cadec1e703eab3c1517cf787
Paul Uhn <paulu@moj.io>
2023-12-06 22:19:07 -0600

    2023 Day 6

---
+++ adventofcode/2023/Event2023Day06.swift
@@ -0,0 +1,40 @@
+//
+
+import Foundation
+
+struct Event2023Day06 {
+    static func part1(_ input: String) -> Int {
+        let regex = /\d{1,4}/
+        let input = input.matches(of: regex).map { $0.int }
+        let time = 0
+        let distance = input.count / 2
+        var wins = [Int]()
+
+        for i in 0..<distance {
+            let t = input[time + i]
+            let d = input[distance + i]
+            wins.append(win(t, d))
+        }
+        return wins.reduce(1, *)
+    }
+
+    static func part2(_ input: String) -> Int {
+        let regex = /\d{1,4}/
+        let input = input.matches(of: regex).map { $0.str }
+        let half = input.count / 2
+
+        let t = input[0..<half].joined().int
+        let d = input[half..<input.count].joined().int
+        return win(t, d)
+    }
+
+    private static func win(_ t: Int, _ d: Int) -> Int {
+        (0...t).map {
+            let speed = $0
+            let timeLeft = t - $0
+            let distance = speed * timeLeft
+            return distance > d ? 1 : 0
+        }
+        .reduce(0, +)
+    }
+}
